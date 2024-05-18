9080937171f920041b425591a9cb829ce0d7fab4
LuciferLovesMe <abhiemsmile@gmail.com>
2024-05-19 05:00:44 +0700

    redesign-ui: fix logout button

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -49,39 +49,45 @@
            <!-- Nav items -->
            <ul class="navbar-nav">
              <li class="nav-item">
-               <a class="nav-link" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-dashboards">
+               <a class="nav-link" href="#navbar-manajemen" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-manajemen">
                  <i class="ni ni-shop text-primary"></i>
-                 <span class="nav-link-text">Dashboards</span>
+                 <span class="nav-link-text">Manajemen Magang</span>
                </a>
-               <div class="collapse" id="navbar-dashboards">
+               <div class="collapse" id="navbar-manajemen">
                  <ul class="nav nav-sm flex-column">
                    <li class="nav-item">
-                     <a href="../../pages/dashboards/dashboard.html" class="nav-link">Dashboard</a>
+                     <a href="#" class="nav-link">Akun Mahasiswa</a>
                    </li>
                    <li class="nav-item">
-                     <a href="../../pages/dashboards/alternative.html" class="nav-link">Alternative</a>
+                     <a href="#" class="nav-link">Proposal Magang</a>
+                   </li>
+                   <li class="nav-item">
+                     <a href="#" class="nav-link">Surat Balasan</a>
+                   </li>
+                   <li class="nav-item">
+                     <a href="#" class="nav-link">Surat Pengantar</a>
                    </li>
                  </ul>
                </div>
              </li>
              <li class="nav-item">
-               <a class="nav-link" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">
+               <a class="nav-link" href="#navbar-master" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-master">
                  <i class="ni ni-ungroup text-orange"></i>
-                 <span class="nav-link-text">Examples</span>
+                 <span class="nav-link-text">Master</span>
                </a>
-               <div class="collapse" id="navbar-examples">
+               <div class="collapse" id="navbar-master">
                  <ul class="nav nav-sm flex-column">
                    <li class="nav-item">
-                     <a href="../../pages/examples/pricing.html" class="nav-link">Pricing</a>
+                     <a href="#" class="nav-link">Dosen</a>
                    </li>
                    <li class="nav-item">
-                     <a href="../../pages/examples/login.html" class="nav-link">Login</a>
+                     <a href="#" class="nav-link">Tempat Magang</a>
                    </li>
                    <li class="nav-item">
-                     <a href="../../pages/examples/register.html" class="nav-link">Register</a>
+                     <a href="#" class="nav-link">FAQ</a>
                    </li>
                    <li class="nav-item">
-                     <a href="../../pages/examples/lock.html" class="nav-link">Lock</a>
+                     <a href="#" class="nav-link">File Template</a>
                    </li>
                    <li class="nav-item">
                      <a href="../../pages/examples/timeline.html" class="nav-link">Timeline</a>
@@ -93,8 +99,6 @@
                </div>
              </li>
            </ul>
-           <!-- Divider -->
-           <hr class="my-3">
          </div>
        </div>
      </div>
@@ -103,7 +107,29 @@
    <div class="main-content" id="panel">
      <!-- Topnav -->
      <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
-       <div class="container-fluid">
+       <div class="container-fluid d-flex justify-content-end align-items-right text-right">
+        <div class="collapse navbar-collapse d-flex justify-content-end align-items-right text-right" id="navbarSupportedContent">
+            <ul class="navbar-nav align-items-center ml-auto ml-md-0">
+                <li class="nav-item dropdown">
+                  <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <div class="media align-items-center">
+                      <span class="avatar avatar-sm rounded-circle">
+                        <img alt="Image placeholder" src="{{ asset('new-assets/img/theme/team-4.jpg')}}">
+                      </span>
+                      <div class="media-body ml-2 d-none d-lg-block">
+                        <span class="mb-0 text-sm  font-weight-bold">John Snow</span>
+                      </div>
+                    </div>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#!" class="dropdown-item">
+                      <i class="ni ni-user-run"></i>
+                      <span>Logout</span>
+                    </a>
+                  </div>
+                </li>
+            </ul>
+        </div>
        </div>
      </nav>
      <!-- Header -->

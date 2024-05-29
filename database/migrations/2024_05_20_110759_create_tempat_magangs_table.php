2aebcde61744549c93fae73cdc85dfa03e78c746
rayrayaray <rayasya.dziqi@gmail.com>
2024-05-29 19:43:14 +0700

    fix field in tempat_magangs table

--- database/migrations/2024_05_20_110759_create_tempat_magangs_table.php
+++ database/migrations/2024_05_20_110759_create_tempat_magangs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,8 +13,8 @@ return new class extends Migration
         Schema::create('tempat_magangs', function (Blueprint $table) {
             $table->id();
             $table->string('nama_tempat')->nullable();
-            $table->string('daerah')->nullable();
-            $table->timestamps();
+            $table->string('posisi')->nullable();
+            $table->string('alamat')->nullable();
         });
     }
 

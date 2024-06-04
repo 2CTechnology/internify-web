c85081351a32bb0bb0a5fa4315f16ff75ff4e1bb
LuciferLovesMe <abhiemsmile@gmail.com>
2024-06-04 22:17:23 +0700

    feat/finishing: parsing data to landing page

--- resources/views/user/component/cardmagang.blade.php
+++ resources/views/user/component/cardmagang.blade.php
@@ -1,114 +1,30 @@
 <div class="container-magang">
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                    <button data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Detail Data
-                    </button>
-            </div>
-        </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
+    @forelse ($data as $item)
+        <div class="card-parent-magang">
+            <div class="card-magang">
+                <div class="header-magang">
+                    <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
+                </div>
+                <div class="content-magang">
+                    <p>{{ $item->nama_tempat }}</p>
+                    <p>{{ $item->head_office }}</p>
+                        <button data-bs-toggle="modal" data-bs-target="#exampleModal{{$item->id}}">
+                            Detail Data
+                        </button>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
+    @empty
+        <div class="card-parent-magang">
+            <div class="card-magang">
+                <div class="header-magang">
+                    <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
+                </div>
+                <div class="content-magang">
+                    <p>Tidak Ada Data</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                    <button>
-                        Detail Data
-                    </button>
-            </div>
-        </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="card-parent-magang">
-        <div class="card-magang">
-            <div class="header-magang">
-                <img src="{{asset('asset-landing/img/intern.png')}}" alt="Foto Tempat Magang">
-            </div>
-            <div class="content-magang">
-                <p>PT Telekomunikasi (Telkom)</p>
-                <p>Surabaya</p>
-                <a href="">
-                    <button>Detail Data</button>
-                </a>
-            </div>
-        </div>
-    </div>
+        
+    @endforelse
 </div>

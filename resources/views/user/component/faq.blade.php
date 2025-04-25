74c5f5ae6a0df720c098394298c32c7a7024a26a
LatansaBima <bima.iris758@gmail.com>
2025-04-25 20:49:47 +0700

    changing color in timeline

--- resources/views/user/component/faq.blade.php
+++ resources/views/user/component/faq.blade.php
@@ -6,29 +6,29 @@
   
       <!-- Bungkus garis di dalam pembungkus relative -->
       <div class="position-absolute start-50 translate-middle-x" style="top: 80px; height: calc(100% - 80px); z-index: 0;">
-        <div class="bg-dark" style="width: 4px; height: 100%;"></div>
+        <div class="bg-secondary" style="width: 4px; height: 100%;"></div>
       </div>
   
       <!-- Timeline Item 1 (Kiri) -->
       <div class="row mb-5 align-items-center position-relative">
         <div class="col-md-6 text-end pe-4 position-relative">
-          <div class="bg-white shadow p-4 border-end border-4 border-warning d-inline-block text-start">
+          <div class="bg-white shadow p-4 border-end border-4 border-info d-inline-block text-start">
             <h5 class="fw-bold">2017</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </div>
         </div>
         <div class="col-md-6 position-relative">
-          <span class="position-absolute top-50 start-0 translate-middle bg-warning rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
+          <span class="position-absolute top-50 start-0 translate-middle bg-primary rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
         </div>
       </div>
   
       <!-- Timeline Item 2 (Kanan) -->
       <div class="row mb-5 align-items-center position-relative">
         <div class="col-md-6 position-relative">
-            <span class="position-absolute top-50 start-100 translate-middle bg-warning rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
+            <span class="position-absolute top-50 start-100 translate-middle bg-primary rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
         </div>
         <div class="col-md-6 ps-4 position-relative">
-          <div class="bg-white shadow p-4 border-start border-4 border-warning d-inline-block text-start">
+          <div class="bg-white shadow p-4 border-start border-4 border-info d-inline-block text-start">
             <h5 class="fw-bold">2016</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </div>
@@ -38,23 +38,23 @@
       <!-- Timeline Item 3 (Kiri) -->
       <div class="row mb-5 align-items-center position-relative">
         <div class="col-md-6 text-end pe-4 position-relative">
-          <div class="bg-white shadow p-4 border-end border-4 border-warning d-inline-block text-start">
+          <div class="bg-white shadow p-4 border-end border-4 border-info d-inline-block text-start">
             <h5 class="fw-bold">2015</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </div>
         </div>
         <div class="col-md-6 position-relative">
-          <span class="position-absolute top-50 start-0 translate-middle bg-warning rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
+          <span class="position-absolute top-50 start-0 translate-middle bg-primary rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
         </div>
       </div>
   
       <!-- Timeline Item 4 (Kanan) -->
       <div class="row mb-5 align-items-center position-relative">
         <div class="col-md-6 position-relative">
-          <span class="position-absolute top-50 start-100 translate-middle bg-warning rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
+          <span class="position-absolute top-50 start-100 translate-middle bg-primary rounded-circle border border-light" style="width: 20px; height: 20px;"></span>
         </div>
         <div class="col-md-6 ps-4 position-relative">
-          <div class="bg-white shadow p-4 border-start border-4 border-warning d-inline-block text-start">
+          <div class="bg-white shadow p-4 border-start border-4 border-info d-inline-block text-start">
             <h5 class="fw-bold">2014</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </div>

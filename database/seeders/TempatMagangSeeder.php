de68ed4c17bb896539d671e110c818e92b5d42ac
LatansaBima <bima.iris758@gmail.com>
2025-05-17 03:35:36 +0700

    change all seeder untuk cicd berulang tanpa menyebabkan duplicate / idempotent

--- database/seeders/TempatMagangSeeder.php
+++ database/seeders/TempatMagangSeeder.php
@@ -1,64 +1,44 @@
-<?php
-
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class TempatMagangSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('tempat_magangs')->insert([
-            'nama_tempat' => 'JV Partner Indonesia',
-            'posisi' => 'Front-End Programmer',
-            'alamat' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'deskripsi_pekerjaan' => 'F Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'kriteria' => 'Problem Solving, Analisis Data, Komunikasi Efektif',
-            'deskripsi_perusahaan' => 'F Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'website' => 'https://www.jvpartner.id/',
-            'employee_size' => 1000,
-            'head_office' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'since' => 2000,
-            'specialization' => 'Affiliate Network Partnership',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        $datas = [
+            [
+                'posisi' => 'Front-End Programmer',
+                'deskripsi_pekerjaan' => 'F Lorem ipsum dolor...',
+            ],
+            [
+                'posisi' => 'Back-End Programmer',
+                'deskripsi_pekerjaan' => 'B Lorem ipsum dolor...',
+            ],
+            [
+                'posisi' => 'Design UI/UX',
+                'deskripsi_pekerjaan' => 'D Lorem ipsum dolor...',
+            ]
+        ];
 
-        DB::table('tempat_magangs')->insert([
-            'nama_tempat' => 'JV Partner Indonesia',
-            'posisi' => 'Back-End Programmer',
-            'alamat' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'deskripsi_pekerjaan' => 'B Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'kriteria' => 'Problem Solving, Analisis Data, Komunikasi Efektif',
-            'deskripsi_perusahaan' => 'B Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'website' => 'https://www.jvpartner.id/',
-            'employee_size' => 1000,
-            'head_office' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'since' => 2000,
-            'specialization' => 'Affiliate Network Partnership',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-
-        DB::table('tempat_magangs')->insert([
-            'nama_tempat' => 'JV Partner Indonesia',
-            'posisi' => 'Design UI/UX',
-            'alamat' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'deskripsi_pekerjaan' => 'D Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'kriteria' => 'Problem Solving, Analisis Data, Komunikasi Efektif',
-            'deskripsi_perusahaan' => 'D Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus fugit quis voluptatem mollitia deleniti ut architecto dolore, illum in debitis sequi nesciunt, blanditiis quae a recusandae. Dolores, expedita dolor.',
-            'website' => 'https://www.jvpartner.id/',
-            'employee_size' => 1000,
-            'head_office' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
-            'since' => 2000,
-            'specialization' => 'Affiliate Network Partnership',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        foreach ($datas as $data) {
+            DB::table('tempat_magangs')->updateOrInsert(
+                [
+                    'nama_tempat' => 'JV Partner Indonesia',
+                    'posisi' => $data['posisi']
+                ],
+                [
+                    'alamat' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
+                    'deskripsi_pekerjaan' => $data['deskripsi_pekerjaan'],
+                    'kriteria' => 'Problem Solving, Analisis Data, Komunikasi Efektif',
+                    'deskripsi_perusahaan' => $data['deskripsi_pekerjaan'],
+                    'website' => 'https://www.jvpartner.id/',
+                    'employee_size' => 1000,
+                    'head_office' => 'Komplek Pertokoan Sawojajar Jalan Danau Toba Blok C22, Sawojajar, Kec. Kedungkandang, Kota Malang, Jawa Timur 56138',
+                    'since' => 2000,
+                    'specialization' => 'Affiliate Network Partnership',
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]
+            );
+        }
     }
 }

b686c5724c005cfaa272de52103acfbbdf73fdcc
LatansaBima <bima.iris758@gmail.com>
2025-06-11 20:46:31 +0700

    fixing secret for CI SSH

--- resources/views/user/component/about.blade.php
+++ resources/views/user/component/about.blade.php
@@ -1,7 +1,7 @@
 <section id="penjelasan">
     <div class="container-tiga">
         <div class="sub-judul-1">
-            <h3>Apa Itu Internify</h3>
+            <h3>Apa Itu Internify?</h3>
             <p>Sistem informasi magang "Internify" hadir sebagai solusi inovatif untuk mengatasi berbagai permasalahan yang sering timbul 
 dalam proses magang. Beberapa masalah umum yang dihadapi antara lain adalah kesulitan dalam matching antara mahasiswa dengan 
 perusahaan yang tepat, kurangnya transparansi dan monitoring dalam proses magang, serta administrasi yang memakan waktu dan 
